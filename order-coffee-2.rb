b09998d35ec8a2acdd401d35e165153e0f632cc3
Junggu Lee <hyde1117@naver.com>
2014-02-20 22:20:25 +0900

    static으로 형태만 출력

--- order-coffee-2.rb
+++ order-coffee-2.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/order.db")
+
+class Order
+	include DataMapper::Resource
+	property :id, Serial
+	property :name, Text, :required => true
+	property :beverage, Text, :required => true
+	property :created_at, DateTime
+	property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
 
 get '/' do
-	"Hello, World!"
+	@orders = Order.all :order => :id.desc
+	@title = 'All Orders'
+	erb :home
 end
